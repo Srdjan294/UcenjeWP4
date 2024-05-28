8a4fa776183155123d3e6862459d037ec17138c1
Srđan Filipović <srdjanfilipovic991@gmail.com>
2024-05-28 10:18:56 +0200

    Subota vježba Ispiti

---
+++ Subota/Ispiti.sql
@@ -0,0 +1,26 @@
+﻿use master;
+go
+drop database if exists ispit;
+go
+create database ispit collate Croatian_CI_AS;
+go
+
+use ispit;
+
+create table IspitniRok(
+sifra int not null primary key identity(1,1),
+predmet varchar(100) not null,
+vrstaIspita varchar(100) not null,
+datum datetime,
+pristupio bit not null
+);
+
+create table Pristupnici(
+sifra int not null primary key identity(1,1),
+ispitniRok int not null,
+student varchar(100) not null,
+brojBodova int not null,
+ocjena int not null
+);
+
+alter table Pristupnici add foreign key (ispitniRok) references IspitniRok(sifra);
