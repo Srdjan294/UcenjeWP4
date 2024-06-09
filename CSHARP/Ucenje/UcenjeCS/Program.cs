c9e9b6e80c6729011de84b06a77d872428026982
Srđan Filipović <srdjanfilipovic991@gmail.com>
2024-06-09 11:55:57 +0200

    Igrali smo se s ispisom

---
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * 
+ * Komentar kroz
+ * više redova
+ * 
+ * 
+ */
+
+Console.WriteLine("Hello, World!");
+
+Console.Write(7);
+
+Console.WriteLine(true);
+
+Console.WriteLine("Srđan")
+
