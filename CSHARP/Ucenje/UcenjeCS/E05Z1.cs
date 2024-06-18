2b2ff5ef1a0dc8554aaf3a43193990b95f35561f
Srđan Filipović <srdjanfilipovic991@gmail.com>
2024-06-18 14:46:17 +0200

    Nadopuna od vježbi u subotu

--- CSHARP/Ucenje/UcenjeCS/E05Z1.cs
+++ CSHARP/Ucenje/UcenjeCS/E05Z1.cs
@@ -18,6 +18,8 @@ namespace UcenjeCS
             niz[2] = 7;
 
             int[] krace = { 7, 7, 7 };
+
+           
         }
     }
 }
--- CSHARP/Ucenje/UcenjeCS/E05Z3.cs
+++ CSHARP/Ucenje/UcenjeCS/E05Z3.cs
@@ -10,7 +10,19 @@ namespace UcenjeCS
     {
         internal static void Izvedi()
         {
-            
+
+            int[,] tablica =
+            {
+                {2,2,2 },
+                {4,5,6 },
+                {2,8,2 }
+
+
+            };
+
+            Console.WriteLine("{0} {1} {2} {3}", tablica[0,0], tablica[2,0], tablica[2,0], tablica[2,2]);
+
+
         }
     }
 }
