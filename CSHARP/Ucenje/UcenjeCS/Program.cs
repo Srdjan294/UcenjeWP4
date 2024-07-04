032ce063e268a59dbe249166ce9c87b29da1bf68
Srdjan294 <srdjanfilipovic991@gmail.com>
2024-07-04 19:00:24 +0200

    Ekstenzije, Lambda, Sučelja, Generici

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -3,7 +3,9 @@ using UcenjeCS;
 using UcenjeCS.E13KlasaObjekt;
 
 
-new UcenjeCS.E15RadSStringovima.Program();
+new UcenjeCS.E16GenericiLambdaEkstenzije.Program();
+
+//new UcenjeCS.E15RadSStringovima.Program();
 
 //new UcenjeCS.E14Nasljedivanje.Program();
 
