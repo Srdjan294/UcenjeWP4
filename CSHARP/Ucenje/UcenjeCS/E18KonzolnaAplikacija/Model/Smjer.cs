17d14380fe10134b97908565ac8b4775a8c077d8
Srđan Filipović <srdjanfilipovic991@gmail.com>
2024-08-09 10:20:18 +0200

    Ispravak: ovo su promjene od zadnjeg predavanja

--- CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/Model/Smjer.cs
+++ CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/Model/Smjer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UcenjeCS.E18KonzolnaAplikacija.Model
+﻿namespace UcenjeCS.E18KonzolnaAplikacija.Model
 {
     internal class Smjer:Entitet
     {
--- CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/Pomocno.cs
+++ CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/Pomocno.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UcenjeCS.E18KonzolnaAplikacija
+﻿namespace UcenjeCS.E18KonzolnaAplikacija
 {
     internal class Pomocno
     {
+
+        public static bool DEV=false;
+
         internal static bool UcitajBool(string poruka, string trueValue)
         {
             Console.Write(poruka + ": ");
@@ -97,6 +94,26 @@ namespace UcenjeCS.E18KonzolnaAplikacija
                 if ( (obavezno && s.Length==0) || s.Length > max)
                 {
                     Console.WriteLine("Unos obavezan, maksimalno dozvoljeno {0} znakova",max);
+                    continue;
+                }
+                return s;
+            }
+        }
+
+        internal static string UcitajString(string stara,string poruka, int max, bool obavezno)
+        {
+            string s;
+            while (true)
+            {
+                Console.Write(poruka + " (" + stara + "): ");
+                s = Console.ReadLine().Trim();
+                if (s.Length == 0)
+                {
+                    return stara;
+                }
+                if ((obavezno && s.Length == 0) || s.Length > max)
+                {
+                    Console.WriteLine("Unos obavezan, maksimalno dozvoljeno {0} znakova", max);
                     continue;
                 }
                 return s;
