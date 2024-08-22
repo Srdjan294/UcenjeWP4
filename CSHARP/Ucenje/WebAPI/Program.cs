4f99f69f09b4b4f8a25be6f6e07357203844dec0
Srdjan294 <srdjanfilipovic991@gmail.com>
2024-08-22 16:56:15 +0200

    Opcije delegacija

--- CSHARP/Ucenje/WebAPI/Program.cs
+++ CSHARP/Ucenje/WebAPI/Program.cs
@@ -15,7 +15,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(opcije =>
     {
-        
+        opcije.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
     }) ;
 }
 
