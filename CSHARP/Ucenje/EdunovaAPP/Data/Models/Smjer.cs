1dce416cbd28c8873982b9d28d2f9d9325671762
Srdjan294 <srdjanfilipovic991@gmail.com>
2024-08-22 18:41:52 +0200

    Spajanje na bazu i rute

---
+++ CSHARP/Ucenje/EdunovaAPP/Data/Models/Smjer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EdunovaAPP.Data.Models
+{
+    public class Smjer: Entitet
+    {
+        public string? Naziv { get; set; }
+        public int? Trajanje { get; set; }
+        public decimal? Cijena { get; set; }
+        public DateTime? IzvodiSeOd { get; set; }
+        [Column("verificiran")]  // Ovo ne morate u svojim završnim radovima raditi
+        public bool? Vaucer { get; set; }
+    }
+}
